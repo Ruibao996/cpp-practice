d04c3a61fddc65caf7bed264c42151cc9494f9d1
Rui <ruifli2022@gmail.com>
2024-04-25 13:03:54 +0800

    day2

---
+++ DataStructure/Array/FindDuplicatedNum.cpp
@@ -0,0 +1,131 @@
+# include <iostream>
+# include <cstring>
+using namespace std;
+
+bool duplicate(int num[], int length, int* dupliation)
+{
+    if (num == nullptr || length <= 0)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < length; ++i)
+    {
+        if (num[i] < 0 || num[i] > length - 1)
+        {
+            return false;
+        }
+    }
+
+    for (int i = 0; i < length; ++i)
+    {
+        while(num[i] != i)
+        {
+            if (num[i] == num[num[i]])
+            {
+                *dupliation = num[i];
+                return true;
+            }
+
+            int temp = num[i];
+            num[i] = num[num[i]];
+            num[temp] = temp;
+        }
+    }
+
+    return false;
+}
+
+bool contains(int array[], int length, int number)
+{
+    for(int i = 0; i < length; ++i)
+    {
+        if(array[i] == number)
+            return true;
+    }
+
+    return false;
+}
+
+void test(const char* testName, int numbers[], int lengthNumbers, int expected[], int expectedExpected, bool validArgument)
+{
+    printf("%s begins: ", testName);
+
+    int duplication;
+    bool validInput = duplicate(numbers, lengthNumbers, &duplication);
+
+    if(validArgument == validInput)
+    {
+        if(validArgument)
+        {
+            if(contains(expected, expectedExpected, duplication))
+                printf("Passed.\n");
+            else
+                printf("FAILED.\n");
+        }
+        else
+            printf("Passed.\n");
+    }
+    else
+        printf("FAILED.\n");
+}
+
+/*test part*/
+
+// 重复的数字是数组中最小的数字
+void test1()
+{
+    int numbers[] = { 2, 1, 3, 1, 4 };
+    int duplications[] = { 1 };
+    test("Test1", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), true);
+}
+
+// 重复的数字是数组中最大的数字
+void test2()
+{
+    int numbers[] = { 2, 4, 3, 1, 4 };
+    int duplications[] = { 4 };
+    test("Test2", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), true);
+}
+
+// 数组中存在多个重复的数字
+void test3()
+{
+    int numbers[] = { 2, 4, 2, 1, 4 };
+    int duplications[] = { 2, 4 };
+    test("Test3", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), true);
+}
+
+// 没有重复的数字
+void test4()
+{
+    int numbers[] = { 2, 1, 3, 0, 4 };
+    int duplications[] = { -1 }; // not in use in the test function
+    test("Test4", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), false);
+}
+
+// 没有重复的数字
+void test5()
+{
+    int numbers[] = { 2, 1, 3, 5, 4 };
+    int duplications[] = { -1 }; // not in use in the test function
+    test("Test5", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), false);
+}
+
+// 无效的输入
+void test6()
+{
+    int* numbers = nullptr;
+    int duplications[] = { -1 }; // not in use in the test function
+    test("Test6", numbers, 0, duplications, sizeof(duplications) / sizeof(int), false);
+}
+
+int main()
+{
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+}
